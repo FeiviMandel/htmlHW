825ba8ede534533dde83f484da2af91e0456be73
Feivi Mandel <feivimandel@gmail.com>
2020-09-08 22:03:56 -0400

    homework 65

---
+++ homework/javascriptHW/HW65/hw.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  const thePage = $('#fileToLoad');
+  const page = $('#page');
+  const theForm = $('#form');
+  theForm.submit(event => {
+    event.preventDefault();
+    fetch(thePage.val())
+      .then(r => r.text())
+      .then((text) => page.text(text))
+      .catch(() => window.pcs.messageBox.show('bad error!'));
+    theForm[0].reset();
+  });
+}());
