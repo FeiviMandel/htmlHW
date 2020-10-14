c2292bc5a4dc6b9b493d5c19e0172572728b58e8
Feivi Mandel <feivimandel@gmail.com>
2020-10-13 23:13:29 -0400

    homework 68 revised

--- homework/javascriptHW/HW68/68next.js
+++ homework/javascriptHW/HW68/68next.js
@@ -9,10 +9,54 @@
     const form = $('#form');
     const page = $('#page');
     const picture = $('#picture img');
-    const title = $('#picture div');
+    const title = $('#picture div');    
+    const a = $('#a');
+    const b = $('#b');
+    const c = $('#c');
+    const d = $('#d');
+    const e = $('#e');
+    const f = $('#f');
+    const g = $('#g');
+    const h = $('#h');
+    const images = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg', 'images/img5.jpg', 'images/img6.jpg', 'images/img7.jpg', 'images/img8.jpg', 'images/img9.jpg', 'images/img10.jpg', 'images/img11.jpg', 'images/img12.jpg', 'images/img13.jpg', 'images/img14.jpg', 'images/img15.jpg', 'images/img16.jpg', 'images/img17.jpg'];
     let index = 0;
-
+    let i = 0;
     page.hide();
+    function flashImages() {
+        a.attr('src', images[i++]);
+        if (i === images.length) {
+            i = 0;
+        }
+        b.attr('src', images[i++]);
+        if (i === images.length) {
+            i = 0;
+        }
+        c.attr('src', images[i++]);
+        if (i === images.length) {
+            i = 0;
+        }
+        d.attr('src', images[i++]);
+        if (i === images.length) {
+            i = 0;
+        }
+        e.attr('src', images[i++]);
+        if (i === images.length) {
+            i = 0;
+        }
+        f.attr('src', images[i++]);
+        if (i === images.length) {
+            i = 0;
+        }
+        g.attr('src', images[i++]);
+        if (i === images.length) {
+            i = 0;
+        }
+        h.attr('src', images[i++]);
+        if (i === images.length) {
+            i = 0;
+        }
+    }
+    setInterval(flashImages, 1500);    
     form.submit(event => {
         page.hide();
         event.preventDefault();
