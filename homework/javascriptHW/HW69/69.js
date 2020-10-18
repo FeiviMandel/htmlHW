23b1cbc2237b558f9887512879a5f27d861655c7
Feivi Mandel <feivimandel@gmail.com>
2020-10-18 11:28:57 -0400

    homework 69

---
+++ homework/javascriptHW/HW69/69.js
@@ -0,0 +1,53 @@
+/* globals google, $ */
+function initMap() {
+    'use strict';
+
+    const form = $('#form');
+    const img = $('#sidebar ul');
+    const search = $('#search');
+    const loc = { lat: 40.095657332825816, lng: -74.22207079649733 };
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: loc,
+        zoom: 2,
+    });
+    const bounds = new google.maps.LatLngBounds();
+    const infoWindow = new google.maps.InfoWindow();
+    form.submit(event => {
+        event.preventDefault();
+        img.empty();
+        $.getJSON(`http://api.geonames.org/wikipediaSearch?q=${search.val()}&maxRows=10&username=feivi&type=json`)
+            .then(x => {
+                x.geonames.forEach(r => {
+                    $(`<li>
+                        <img src="${r.thumbnailImg}" alt="${r.title}">
+                        <span id="title">${r.title}<span><div id= "summary">${r.summary}</div>
+                    </li>`)
+                        .appendTo(img)
+                        .click(function () {
+                            console.log(r);
+                            map.panTo(pos);
+                            map.setZoom(8);
+                        });
+                    let pos = { lat: r.lat, lng: r.lng };
+                    bounds.extend(pos);
+                    let marker = new google.maps.Marker({
+                        position: pos,
+                        map: map,
+                        title: (`${r.title}`)
+                    });
+                    if (r.thumbnailImg) {
+                        marker.setIcon({
+                            url: (`${r.thumbnailImg}`),
+                            scaledSize: new google.maps.Size(40, 40)
+                        });
+                    }
+                    marker.addListener('click', () => {
+                        infoWindow.setContent(`${r.summary}<br><a target="_blank" href=https://${r.wikipediaUrl}>more info</a>`);
+                        infoWindow.open(map, marker);
+                    });
+                });
+                map.fitBounds(bounds);
+            })
+            .catch(e => console.error(e));
+    });
+}
