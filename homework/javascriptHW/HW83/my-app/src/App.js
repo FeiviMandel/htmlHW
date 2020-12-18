4bd6e0019a7fae74027ca8ce93cf506440652409
Feivi Mandel <feivimandel@gmail.com>
2020-12-17 22:35:32 -0500

    homework 83

---
+++ homework/javascriptHW/HW83/my-app/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import React, { Component } from 'react';
+import WeatherDetails from './WeatherDetails';
+
+export default class App extends Component {
+  state = {};
+  getTheWeather = e => {
+    const units = 'imperial';
+    const zip = e.target.value;
+    const apiKey = 'a4f4d42bbc7552a93d345f9347e65a23';
+    fetch(`http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&zip=${zip}&units=${units}`)
+      .then(r => {
+        if (!r.ok) {
+          // throw new Error(`${r.status} ${r.statusText}`);
+          throw new Error('Zipcode Not Found. Please Enter A Valid Zipcode');
+        }
+        return r.json();
+      })
+      .then(weatherData => {
+        this.setState({
+          weather: {
+            location: weatherData.name,
+            picture: `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
+            description: `${weatherData.main.temp} and ${weatherData.weather[0].description}`
+          }
+        });
+      })
+      .catch(err => alert(err));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>PCS Weather App</h1>
+        <WeatherDetails weather={this.state.weather} />
+        <input type="number" onBlur={this.getTheWeather} />
+      </div>
+    );
+  }
+}
---
+++ homework/javascriptHW/HW83/my-app/src/WeatherDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function WeatherDetails(props) {
+    // 'use strict';
+    if (props.weather) {
+        const { location, description, picture } = props.weather;
+        return (
+            <>
+                <h2>{location}</h2>
+                <img src={picture} alt={location} />
+                <h3>{description}</h3>
+            </>
+        );
+    }
+    return <h3>Please Enter A Zipcode For The Current Weather</h3>
+}
